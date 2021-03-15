4ad56170d4521a76242d2f365dbfcdf6db772dc6
gyw <mas_gyw@163.com>
2021-03-15 17:46:45 +0800

    feat:init logging-tools project
    create logging-tools to upload jar and download jar from remote server

--- platform/logging-tools/src/main/java/cn/gyw/platform/tools/logging/model/RemoteConnect.java
+++
@@ -1,9 +0,0 @@
-package cn.gyw.platform.tools.logging.model;
-
-import java.io.Serializable;
-
-public class RemoteConnect implements Serializable {
-
-	private static final long serialVersionUID = 1L;
-
-}
