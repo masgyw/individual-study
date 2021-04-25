0f7e35298e4212e264acfe52f898b7b4ecde7526
gyw <mas_gyw@163.com>
2021-04-25 18:06:51 +0800

    feat:learn redis

---
+++ frameworks/springboot/src/main/java/cn/gyw/springboot/redis/RedisConfig.java
@@ -0,0 +1,34 @@
+package cn.gyw.springboot.redis;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+@Configuration
+@ComponentScan(value = "cn.gyw.springboot.redis")
+public class RedisConfig {
+
+	@Bean
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+
+        redisTemplate.setKeySerializer(RedisSerializer.string());
+        redisTemplate.setValueSerializer(RedisSerializer.java());
+        
+        return redisTemplate;
+    }
+	
+	@Bean
+	@ConditionalOnMissingBean
+	public StringRedisTemplate stringRedisTemplate(LettuceConnectionFactory connectionFactory) {
+		StringRedisTemplate template = new StringRedisTemplate();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+	}
+}
