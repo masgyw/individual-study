a5768ed4f8cbdc0758a784ee3e3a36eb026471b0
gyw <1306501974@qq.com>
2021-06-01 22:41:17 +0800

    feat:add gradle plugin for MGB

--- platform/mybatis-generator-plus/src/test/java/cn/gyw/platform/mbg/plus/TkMybatisGenerator.java
+++ platform/mybatis-generator-plus/src/test/java/cn/gyw/platform/mbg/plus/TkMybatisGenerator.java
@@ -1,6 +1,14 @@
 package cn.gyw.platform.mbg.plus;
 
 import org.junit.Test;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 代码生成
@@ -10,7 +18,18 @@ import org.junit.Test;
 public class TkMybatisGenerator {
 
 	@Test
-	public void commonGenerate() {
-		
+	public void commonGenerate() throws Exception {
+		ClassLoader classloader = MbgApplication.class.getClassLoader();
+		InputStream inputStream = classloader.getResourceAsStream("config-tk-common.xml");
+
+		List<String> warnings = new ArrayList<>();
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(inputStream);
+
+		DefaultShellCallback callback = new DefaultShellCallback(true);
+		MyBatisGenerator generator = new MyBatisGenerator(config, callback, warnings);
+
+		generator.generate(null);
+		System.out.println(">>>> generate success!");
 	}
 }
